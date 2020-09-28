2fe11372aa7d9a862df1c46d54a91c8f36e38f95
jpromanonet <prograaming@jpromano.net>
2020-09-28 11:33:24 -0300

    Calling libraries

---
+++ converter.py
@@ -0,0 +1,7 @@
+# Calling all libraries needed
+#-------------------------------
+# Install the following libraries:
+#   pip install fpdf
+#-------------------------------
+from os import listdir
+from fpdf import FPDF
